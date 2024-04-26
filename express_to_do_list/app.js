f4ab22288f82933760c05f38de1911042053ea2d
Lein0rb4gX <lein0rb4g@gmail.com>
2024-04-26 20:44:14 -0300

    new file:   .gitignore new file:   express_to_do_list/app.js new file:   express_to_do_list/package-lock.json new file:   express_to_do_list/package.json new file:   express_to_do_list/src/routes/checkList.js new file:   http_server/server.js

---
+++ express_to_do_list/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const checkLists = require("./src/routes/checkList")
+const app = express();
+
+app.use(express.json())
+app.use("/checkList", checkLists)
+
+app.listen(3000, () => console.log("Server is ON"))
